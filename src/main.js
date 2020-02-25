12021109900f4a54c76a10a6cac15288bcf190e4
xiaowu <fu-wei.wu@qq.com>
2020-02-25 13:47:15 +0800

    完成登录功能模块

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+// 引入全局样式表
+import './assets/css/global.css'
+// 引入element插件
+import './plugins/element'
+
+// 引入发起请求的axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL='http://106.12.11.162:8888/api/private/v1/'
+Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const router = new Router({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/home',
+            name: 'Home',
+            component: Home
+        }
+    ]
 })
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+    // to 将要访问的路径
+    // from 代表从哪个路径跳转而来
+    // next 是一个函数，表示放行
+    // next()放行    next('/login')强制跳转
+
+    if(to.path === '/login') return next();
+    //从sessionStorage中获取到保存的token值
+    const tokenStr = window.sessionStorage.getItem('token');
+    // 如果tokenStr是空的
+    if(!tokenStr)  return next('/login');
+    
+    next();
+})
+
+export default router
