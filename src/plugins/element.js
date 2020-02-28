2b55fbf1d3dbbc6a60618eb616e92435af3dd0fc
xiaowu <fu-wei.wu@qq.com>
2020-02-28 20:08:30 +0800

    订单模块完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 // 按需导入Element组件
-import { Button,Form, FormItem,Input,Message,Container,Header,Aside,Main,Menu,Submenu,MenuItem,Breadcrumb,BreadcrumbItem,Card, Row,Col,Table,TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,Option,Select,Cascader,Alert,Tabs,TabPane,Steps,Step,Checkbox,CheckboxGroup,Upload } from 'element-ui';
+import { Button,Form, FormItem,Input,Message,Container,Header,Aside,Main,Menu,Submenu,MenuItem,Breadcrumb,BreadcrumbItem,Card, Row,Col,Table,TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,Option,Select,Cascader,Alert,Tabs,TabPane,Steps,Step,Checkbox,CheckboxGroup,Upload,Timeline,TimelineItem } from 'element-ui';
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -36,6 +36,8 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 // Message需要全局挂载
 Vue.prototype.$message = Message
